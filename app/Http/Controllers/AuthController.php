dfdce4134aa09b5ec6012bbfaf7d1c0ece11986b
Aditya <ashishkumar2000ind@gmail.com>
2026-04-13 12:07:41 +0530

    Auth apis refactored

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Testing\Fluent\Concerns\Has;
 
 class AuthController extends Controller
 {
